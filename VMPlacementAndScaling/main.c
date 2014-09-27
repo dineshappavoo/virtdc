fa2c5c3b6a6b2a1e2a3aa1c5eb83c8cf32321526
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-26 19:38:11 -0500

    Initial Commit

---
+++ VMPlacementAndScaling/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  VMPlacementAndScaling
+//
+//  Created by Dinesh Appavoo on 9/26/14.
+//  Copyright (c) 2014 Dinesh Appavoo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
