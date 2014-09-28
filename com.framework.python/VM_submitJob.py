32037ed0591779f7e4fecfc5d76a5aef3aa66e11
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-27 23:44:25 -0500

    Updated the submit job function to create new VM

--- com.framework.python/VM_submitJob.py
+++ com.framework.python/VM_submitJob.py
@@ -1,36 +1,64 @@
 #!/usr/bin/python
-
 import sys, getopt, subprocess
-
 def main(argv):
-    cpu= ''
-    memory = ''
-    vmid = ''
-    time = ''
-    try:
-        opts, args = getopt.getopt(argv,"vmid:cpu:mem:time:",["vmid=","cpu=","mem=","time="])
-    except getopt.GetoptError:
-        print 'VM_submitjob.py --vmid <VMID> --cpu <CPU> --mem <MEMORY> --time <TIME>'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '--h':
-            print 'VM_submitjob.py --vmid <VMID> --cpu <CPU> --mem <MEMORY> --time <TIME>'
-            sys.exit()
-        elif opt in ("--cpu", "-cpu"):
-            cpu = arg
-        elif opt in ("--mem", "-mem"):
-            memory = arg
-        elif opt in ("--vmid", "-vmid"):
-            vmid = arg
-        elif opt in ("--time", "-time"):
-            time = arg
+	cpu= ''
+	memory = ''
+	vmid = ''
+	time = ''
+	max_memory=4194304
+	try:
+		opts, args = getopt.getopt(argv,"vmid:cpu:mem:time:",["vmid=","cpu=","mem=","time="])
+	except getopt.GetoptError:
+		print 'VM_submitjob.py --vmid <VMID> --cpu <CPU> --mem <MEMORY> --time <TIME>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '--h':
+			print 'VM_submitjob.py --vmid <VMID> --cpu <CPU> --mem <MEMORY> --time <TIME>'
+			sys.exit()
+		elif opt in ("--cpu", "-cpu"):
+			cpu = arg
+		elif opt in ("--mem", "-mem"):
+			memory = arg
+		elif opt in ("--vmid", "-vmid"):
+			vmid = arg
+		elif opt in ("--time", "-time"):
+			time = arg
     
-    print subprocess.call("date")
+	#To get the guest os configuration xml for the first time
+	#cmd = "virsh dumpxml Test_clone > /root/Desktop/PYTHON/guestconfig.xml"
+	#p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
     
-    print 'VMID "', vmid
-    print 'CPU "', cpu
-    print 'memory"', memory
-    print 'time"', time
-
+	#command to get the xml into python string for further updates. guestconfig.xml needs to be copied(physically present) int the same folder of this script
+	nodeInfo="cat /root/Desktop/PYTHON/guestconfig.xml"
+	xmlstring = subprocess.check_output(nodeInfo, shell=True, stderr=subprocess.PIPE)
+    
+	#command to copy the iso image to the destination. Every VM will have an individual iso image. (I think this copy can be cleared later on). Make the nodes passwordless
+	image_dest="node3:/var/lib/libvirt/images/"+vmid+".img"
+	cp_cmd = "scp node1:/var/lib/libvirt/images/Test.img"+image_dest
+	copy_image = subprocess.Popen(cp_cmd, shell=True, stderr=subprocess.PIPE)
+    
+	#config update based on the new VM requiement  	#image_path	max_memory	current_memory	current_cpu	max_cpu
+	xmlstring=xmlstring.replace("vm_name", vmid);
+	xmlstring=xmlstring.replace("max_memory", memory);
+	xmlstring=xmlstring.replace("current_memory", memory);
+	xmlstring=xmlstring.replace("current_cpu", "1");
+	xmlstring=xmlstring.replace("max_cpu", cpu);
+	xmlstring=xmlstring.replace("image_path", image_dest);
+    
+	print xmlstring
+    
+	#command to clone the image
+	clone="virsh --connect qemu+ssh://node3/system create ./node_info.xml"
+	clone_out = subprocess.Popen(clone, shell=True, stderr=subprocess.PIPE)
+    
+    
+	#subprocess.call(cmd)
+	
+	#subprocess.call(["virsh","dumpxml",">","/Desktop/PYTHON/guestconfig.xml"])
+	print subprocess.call("date")
+	print 'VMID "', vmid
+	print 'CPU "', cpu
+	print 'memory"', memory
+	print 'time"', time
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
