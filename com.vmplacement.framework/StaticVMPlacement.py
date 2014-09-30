2c68e06c5e751b834665ecb14a6abff0c67e01a6
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-30 05:36:41 -0500

    Updated the description

--- com.vmplacement.framework/StaticVMPlacement.py
+++ com.vmplacement.framework/StaticVMPlacement.py
@@ -11,7 +11,7 @@ from Host_machine_info_tracker import Node
 # for doing some other stuff so we have to declare them here.
 #name                = "vmplacementandscaling"
 #version             = "0.1"
-#long_description    = """vmplacementandscaling is a set of API's/tools written to create virtual machines for cloud user efficiently."""
+#long_description    = """vmplacementandscaling is a set of API's/tools written to create virtual machines for cloud users efficiently."""
 #author              = "Dinesh Appavoo"
 #author_email        = "dinesha.cit@gmail.com"
 #url                 = "https://github.com/dineshappavoo/VMPlacementAndScaling"
