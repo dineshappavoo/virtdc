c98b0afa9395bf7e6aafd62cb3129652ad10e8dc
deanmax <deanmax@gmail.com>
2014-10-18 03:19:14 -0500

    Update setupnfs.sh

--- com.vmplacement.setup/setupnfs.sh
+++ com.vmplacement.setup/setupnfs.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/sh
-mkdir -p /exports/images
+########################
+# On NFS server (node1)
+########################
+mkdir /home/nfs
+mkdir /home/vm_img
 cat >>/etc/exports <<EOF
-/exports/images    192.168.122.0/24(rw,no_root_squash)
+/home/nfs node1(rw,sync,no_root_squash) node2(rw,sync,no_root_squash) node3(rw,sync,no_root_squash) node4(rw,sync,no_root_squash)
 EOF
 exportfs -a
 systemctl restart nfs-server.service
 systemctl status nfs-server.service
+mount node1:/home/nfs /home/vm_img
+setsebool -P virt_use_nfs on
+getsebool virt_use_nfs
+
+#################
+# On NFS Clients
+#################
+systemctl restart nfs-server.service
+systemctl status nfs-server.service
+mkdir /home/vm_img
+mount node1:/home/nfs /home/vm_img
+setsebool -P virt_use_nfs on
+getsebool virt_use_nfs
+
 
 #cat >>/etc/fstab <<EOF
 #node1:/exports/images  /var/lib/libvirt/images  nfs  auto  0 0
