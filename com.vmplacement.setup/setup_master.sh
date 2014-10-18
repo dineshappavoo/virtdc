91b77a848dde40844b805a42a1a323a1f175136d
deanmax <deanmax@gmail.com>
2014-10-18 18:17:11 -0500

    split setup into master and client separately

--- com.vmplacement.setup/setup_master.sh
+++ com.vmplacement.setup/setup_master.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 ########################
 # On NFS server (node1)
 ########################
@@ -8,25 +8,10 @@ cat >>/etc/exports <<EOF
 /home/nfs node1(rw,sync,no_root_squash) node2(rw,sync,no_root_squash) node3(rw,sync,no_root_squash) node4(rw,sync,no_root_squash)
 EOF
 exportfs -a
+systemctl stop firewalld
 systemctl restart nfs-server.service
 systemctl status nfs-server.service
 mount node1:/home/nfs /home/vm_img
 setsebool -P virt_use_nfs on
 getsebool virt_use_nfs
-
-#################
-# On NFS Clients
-#################
-systemctl restart nfs-server.service
-systemctl status nfs-server.service
-mkdir /home/vm_img
-mount node1:/home/nfs /home/vm_img
-setsebool -P virt_use_nfs on
-getsebool virt_use_nfs
-
-
-#cat >>/etc/fstab <<EOF
-#node1:/exports/images  /var/lib/libvirt/images  nfs  auto  0 0
-#EOF
-#mount /var/lib/libvirt/images
 
