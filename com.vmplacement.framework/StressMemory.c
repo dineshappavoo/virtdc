4c4579dcd3ffdc14656e80e0dfe181c78cf89e93
dineshappavoo <dinesha.cit@gmail.com>
2014-10-18 18:39:06 -0500

    Stress memory argument commit message for your changes. Lines starting

--- com.vmplacement.framework/StressMemory.c
+++ com.vmplacement.framework/StressMemory.c
@@ -1,83 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main()
-{
-	#define l_BUFLEN_MAX 80
-	int nmeg,l_retval,tot_megs =0;
-	char *ptr,buf[128];
-	char *t;
-	int reqdMegs, n;
-	int noOfMallocs =1;
-	int leftOver=0;
-	tot_megs=0;
-	printf("Required Megs: ");
-	ptr=fgets(buf,20,stdin);
-	if (ptr==NULL) 
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <time.h>
+	int main(int argc, char *argv[])
 	{
-		printf("\n\n");
-		exit (0);
-	}
-	reqdMegs=atoi(buf);
-	if(reqdMegs>2047)
-	{
-	  noOfMallocs = reqdMegs/2047 + 1;
-	  leftOver = reqdMegs%2047;
-	  n=2047; 
-	}
-	int nmegs[noOfMallocs];
-	int i;
-	
-	
-	nmeg=n*(1024*1024);
-	
-	void *mptr[noOfMallocs];
-	for(i=0;i<noOfMallocs-1;i++)	
-	{
-	 mptr[i]=malloc(nmeg);
-	}
-	if(leftOver>0)
-	{
-		mptr[i]=malloc(leftOver*(1024*1024));
-	}
-	int j=0;
-	while (1) {
-	for(i=0;i<noOfMallocs;i++)		
-	{
-	if(tot_megs<reqdMegs)	 
-	 {
-		tot_megs=(tot_megs+n);
-	 }
-	if (mptr == NULL)
-	{
+		if(argc!=2)
+		{
+		    printf( "usage: %d <Memory in Kbs>", argc );
+		}
+
+		int nmeg,l_retval,tot_megs =0;
+		char *ptr,buf[128];
+		char *t;
+		int reqdMegs, n;
+		int noOfMallocs =1;
+		int leftOver=0;
+		tot_megs=0;
 		
-		/* Error message */
-		printf("malloc %d more megs failed (%d total)\n",n,tot_megs);
-		exit(1);
-	}
-	else 
-	{
-		if(tot_megs<reqdMegs)		
+		if (ptr==NULL) 
 		{
-			printf("%d megs additional RAM, Total megs = %d \n\n",n,tot_megs);
+			printf("\n\n");
+			exit (0);
 		}
-		else if(tot_megs != reqdMegs)
+		reqdMegs=atoi(argv[1])/1024;
+		if(reqdMegs>2047)
 		{
-			tot_megs = tot_megs-n+leftOver;			
-			printf("%d megs additional RAM, Total megs = %d \n\n",leftOver,tot_megs);
+		  noOfMallocs = reqdMegs/2047 + 1;
+		  leftOver = reqdMegs%2047;
+		  n=2047; 
 		}
-	}
-
-	 for (j=0;j<nmeg;j+=1024)
+		else
 		{
-			*((char *)mptr[i]+j)='!';
+		  n=reqdMegs;
 		}
+		int nmegs[noOfMallocs];
+		int i;
 	
-}
-sleep(10);
-	} /* while */
+	
+		nmeg=n*(1024*1024);
+	
+		void *mptr[noOfMallocs];
+		for(i=0;i<noOfMallocs;i++)	
+		{
+		 mptr[i]=malloc(nmeg);
+		}
+		if(leftOver>0)
+		{
+			mptr[i]=malloc(leftOver*(1024*1024));
+		}
+		int j=0;
+		while (1) {
+		for(i=0;i<noOfMallocs;i++)		
+		{
+		if(tot_megs<reqdMegs)	 
+		 {
+			tot_megs=(tot_megs+n);
+		 }
+		if (mptr == NULL)
+		{
+		
+			/* Error message */
+			printf("malloc %d more megs failed (%d total)\n",n,tot_megs);
+			exit(1);
+		}
+		else 
+		{
+			if(tot_megs<reqdMegs)		
+			{
+				//printf("%d megs additional RAM, Total megs = %d \n\n",n,tot_megs);
+			}
+			else if(tot_megs != reqdMegs)
+			{
+				tot_megs = tot_megs-n+leftOver;			
+				//printf("%d megs additional RAM, Total megs = %d \n\n",leftOver,tot_megs);
+			}
+		}
 
-	printf("\n\n");
-	return 0;
-} /* main */
+		 for (j=0;j<nmeg;j+=1024)
+			{
+				*((char *)mptr[i]+j)='!';
+			}
+	
+	}
+	sleep(10);
+		} /* while */
+
+		printf("\n\n");
+		return 0;
+	} /* main */
 
