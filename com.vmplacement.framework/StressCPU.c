8d4d3400ec6bbba0f4a640c760e17ee62c29c449
dineshappavoo <dinesha.cit@gmail.com>
2014-10-18 19:04:28 -0500

    added stress cpu

--- com.vmplacement.framework/StressCPU.c
+++ com.vmplacement.framework/StressCPU.c
@@ -5,6 +5,7 @@ int main() {
     int current = 1;
     int totalPrimes = 0;
     int threshold = 1000000;
+    while(1) {
 #pragma omp parallel for schedule(dynamic) reduction(+ : totalPrimes)
     for (current = 1; current <= threshold; current++) { 
         int i = 2; 
@@ -15,6 +16,7 @@ int main() {
         }
         if(i == current)
             totalPrimes++;
+    }
     }
     printf("%d prime numbers under %d\n",totalPrimes,threshold);
     return 0;
