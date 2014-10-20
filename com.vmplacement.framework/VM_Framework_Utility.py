65416719fc7361d2099d6ba5bb4983643697b1f2
dineshappavoo <dinesha.cit@gmail.com>
2014-10-20 02:16:09 -0500

    getGuestIP() can now support remote vm

--- com.vmplacement.framework/VM_Framework_Utility.py
+++ com.vmplacement.framework/VM_Framework_Utility.py
@@ -5,6 +5,7 @@ This script has the utility functions for the VM framework
 import pexpect
 import sys
 import pickle
+from time import sleep
 
 
 #==============================================================================
@@ -20,24 +21,36 @@ import pickle
 
 #==============================================================================
 
-def getGuestIP(vmid, username, password) : 
+def getGuestIP(host, vmid, username, password) : 
 	#vmid = "Test_node1"
 	#username = "root"
 	#password = "Teamb@123"
 	
-	child = pexpect.spawn('/usr/bin/virsh console ' + vmid)
+	# Ensure ssh to target host is passwordless
+	child = pexpect.spawn('/usr/bin/ssh ' + host)
+	child.expect('\~\]\#')
+	child.sendline('/usr/bin/virsh console ' + vmid)
 	child.sendline('\n\n')
-	child.expect('ogin: ')
-	child.sendline(username)
-	child.expect('assword:')
-	child.sendline(password)
-	child.expect(']# ')
-	#To  get the guest IP
-	child.sendline('ifconfig | grep 192.168 | awk \'{print $2}\'')
+	i = child.expect([pexpect.TIMEOUT, '\~\]\#', 'ogin: '])
+	if i == 0:  # timeout
+		return
+	if i == 1:  # ttyS0 is logged in, do nothing
+		pass
+	if i == 2:  # ttyS0 is waiting at login prompt
+		child.expect('ogin: ')
+		child.sendline(username)
+		child.expect('assword:')
+		child.sendline(password)
+		child.expect('\~\]\# ')
+
+	#To get the guest IP
+	child.sendline('ifconfig | grep 192.168.1. | awk \'{print $2}\'')
 	child.expect('(\d+\.\d+\.\d+\.\d+)')
 	ip = child.match.group()
 
 	child.sendline('logout')
+	#sleep(2)
+	child.sendline('\n\n')
 	print ip
 	return ip
 
