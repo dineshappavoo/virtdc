da7cee9f6e52b3fa9937f2e2a5a48ea424071c42
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-10-20 05:10:46 -0500

    updated simulation code and disk size in xml

--- com.vmplacement.simulation/simulate_google_data.py
+++ com.vmplacement.simulation/simulate_google_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-import os, sys
+import os, sys, subprocess
+sys.path.append('../VMPlacementAndScaling/com.vmplacement.framework')
+
 #==============================================================================
 # Variables
 #==============================================================================
@@ -13,11 +15,28 @@ import os, sys
 #==============================================================================
 
 data_folder_path='../com.vmplacement.data/vms'
-for i in os.listdir(data_folder_path):
-    if i.endswith(".csv") or i.endswith(".py"): 
-        print i
-        continue
-    else:
-        continue
 
+def simulate_google_data():
+    for file_name in os.listdir(data_folder_path):
+        if file_name.endswith(".csv"):
+            #print file_name
+            execute_task(file_name)
+            continue
+        else:
+            continue
 
+def execute_task(file_name):
+    cpu_Val_cmd = 'tail -2 '+data_folder_path+'/'+file_name +' | head -1'
+    cpu_val = subprocess.check_output(cpu_Val_cmd, shell=True, stderr=subprocess.PIPE)
+    cpu_val=cpu_val.strip()
+    mem_Val_cmd = 'tail -1 '+data_folder_path+'/'+file_name
+    mem_val = subprocess.check_output(mem_Val_cmd, shell=True, stderr=subprocess.PIPE)
+    mem_val = mem_val.strip()
+    disk = '5242880'
+    vmid=file_name[:-4]
+    create_vm_cmd='./virtds.py --vmid '+vmid+' --cpu '+cpu_val+' --mem '+mem_val+' --io 5242880'
+    print create_vm_cmd+'\n'
+    #os.system(create_vm_cmd) #5 GB disk by default
+                      
+
+simulate_google_data()
