ce8476875aa7799829c575bb2675efb4d55f47c0
dineshappavoo <dinesha.cit@gmail.com>
2014-11-04 18:22:25 -0600

    Updated email module

--- mail/email_test.py
+++ mail/email_test.py
@@ -1,24 +1,29 @@
+#!/usr/bin/env python
 # Import smtplib for the actual sending function
 import smtplib
 
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 
-# Open a plain text file for reading.  For this example, assume that
-# the text file contains only ASCII characters.
-fp = open('/Users/Dany/Documents/VMPlacementAndScaling/VMPlacementAndScaling/mail/textfile.py', 'rb')
-# Create a text/plain message
-msg = MIMEText(fp.read())
-fp.close()
+def send_support_mail():
+	# Open a plain text file for reading.  For this example, assume that
+	# the text file contains only ASCII characters.
+	fp = open('/var/lib/virtdc/mail/textfile.py', 'rb')
+	# Create a text/plain message
+	msg = MIMEText(fp.read())
+	fp.close()
 
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'The contents of Test mail'
-msg['From'] = 'dinesha.cit@gmail.com'
-msg['To'] = 'dinesha.cit@gmail.com'
+	from_address = 'virtdc-support@virtdc.org'
+	to_address = ['dinesh.appavoo@utdallas.edu','imaginejhm@gmail.com','rxw130330@utdallas.edu','qxd130530@utdallas.edu','rkn130030@utdallas.edu']
+	msg['Subject'] = 'Test mail from virtdc application support'
+	msg['From'] = from_address
+	msg['To'] = ",".join(to_address)
 
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-s = smtplib.SMTP('localhost')
-s.sendmail(me, [you], msg.as_string())
-s.quit()
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+	s = smtplib.SMTP('localhost')
+	s.sendmail(from_address, to_address, msg.as_string())
+	s.quit()
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+	send_support_mail()
--- mail/textfile.py
+++ mail/textfile.py
@@ -1,1 +1,1 @@
-This is test mail
+This is test mail from virtdc application support. VM health will be reported through email
