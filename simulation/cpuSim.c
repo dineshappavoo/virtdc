b9eed2757214c83ceb01c32cbec7cb0c6ff7470b
= <=>
2014-11-19 18:42:50 -0600

    cpuSim.c

--- simulation/cpuSim.c
+++ simulation/cpuSim.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[]){
 
 				end=clock();
 				time=(double)(end-start)/(CLOCKS_PER_SEC);
-				printf("core: %d process ID: %d usage: %.2f calc time: %.2f\n", cores, myId, cpuUsage, time);
+				// printf("core: %d process ID: %d usage: %.2f calc time: %.2f\n", cores, myId, cpuUsage, time);
 
 				usleep(sleepTime);
 				elapsedTime++;
