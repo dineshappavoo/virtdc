6c9c04a3b5aaba6487f1fd1581ce7e4741392cd6
= <=>
2014-11-25 16:59:09 -0600

    new cpu sim.

--- simulation/cpuSim.c
+++ simulation/cpuSim.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
 	pid_t *pids = (pid_t*)malloc(sizeof(pid_t)*cores);
 
 	int i=0;
-	long magicPer01 = 5373779; 
+	long magicPer01 = 4233779; 
 	
 	int ratioCpu;
 	int sleepTime;
@@ -30,23 +30,35 @@ int main(int argc, char *argv[]){
 	clock_t end;
 	double time;
 
-	int a=1;
-	int b=2;
-	int c=1;
+	FILE *tax_info = fopen("tax_info.txt", "r");
+	FILE *tax_report = fopen("tax_report.txt", "w+");
+
+	int tax_state;
+	int tax_fed;
+	int tax_total;
+
+	int *tax_additional = (int*)malloc(sizeof(int)*3);
+	tax_additional[0]=2;
+	tax_additional[1]=4;
+	tax_additional[2]=0;
 
 	for(i=0; i<cores; i++){
 		pids[i]=fork();
 		if(pids[i]==0){
 			while(1){
-				long myId=(long)getpid();
+				//long myId=(long)getpid();
 
 				start=clock();
+				fscanf(tax_info, "%d %d", &tax_state, &tax_fed);
+
 				ratioCpu = (int)(cpuUsage*100);
 				sleepTime = (100-ratioCpu)*10000;
 				for(i=0; i<magicPer01*ratioCpu; i++){
-					c=a*b;
+					tax_total=tax_state+tax_fed;
+					tax_additional[2]=tax_additional[0] * tax_additional[1];
+					tax_total=tax_total+tax_additional[2];
 				}
-
+				fprintf(tax_report, "%d\n", tax_total);
 				end=clock();
 				time=(double)(end-start)/(CLOCKS_PER_SEC);
 				//printf("core: %d process ID: %d usage: %.2f calc time: %.2f\n", cores, myId, cpuUsage, time);
@@ -60,9 +72,13 @@ int main(int argc, char *argv[]){
 					return 0;
 				}
 			}
+			fclose(tax_info);
+			fclose(tax_report);
 			return 0;
 		}
 	}
 
+	fclose(tax_info);
+	fclose(tax_report);
 	return 0;
 }
