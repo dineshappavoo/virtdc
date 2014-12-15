2fd33258587ebae6fb873c30bb6c95fb960e3707
dineshappavoo <dinesha.cit@gmail.com>
2014-12-14 21:38:55 -0600

    typo correction: socketet to socket

--- virtdc_init/virtdc_init.py
+++ virtdc_init/virtdc_init.py
@@ -20,12 +20,12 @@ def virtdc_init():
 	cmd = "/usr/bin/python /var/lib/virtdc/framework/VM_terminationHandler.py &"
 	termination_process = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
 
-	delete_log_files_cmd = "/usr/bin/python /var/lib/virtdc/vmonere/host/vmonere_exec_log_deleter.py &"
-	delete_log_files_process = subprocess.Popen(delete_log_files_cmd, shell=True, stderr=subprocess.PIPE)
-
-	#initia the vmonere socket listener
+	#initiate the vmonere socket listener
 	vmonere_socket_listener_cmd = "/usr/bin/python /var/lib/virtdc/vmonere/sockets/vmonere_listener.socket.py &"
 	vmonere_socket_listener = subprocess.Popen(vmonere_socket_listener_cmd, shell=True, stderr=subprocess.PIPE)
+
+	delete_log_files_cmd = "/usr/bin/python /var/lib/virtdc/vmonere/host/vmonere_exec_log_deleter.py &"
+	delete_log_files_process = subprocess.Popen(delete_log_files_cmd, shell=True, stderr=subprocess.PIPE)
 
 if __name__ == "__main__":
    # stuff only to run when not called via 'import' here
