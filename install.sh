ea0b14510776c065178238c6c67e1dce184517f3
dineshappavoo <dinesha.cit@gmail.com>
2014-12-15 19:29:41 -0600

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 su -
-alias virtdc=/var/lib/virtdc/usr_bin/virtdc.py
-chmod 755 /var/lib/virtdc/usr_bin/virtdc.py
+sudo alias virtdc=/var/lib/virtdc/usr_bin/virtdc.py
+sudo cp /var/lib/virtdc/usr_bin/virtdc.py /usr/bin/virtdc
+sudo chmod 755 /var/lib/virtdc/usr_bin/virtdc.py
+sudo chmod 755 /usr/bin/virtdc
+
